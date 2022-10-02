5b5f38e4d20d67aceaf8b0444de0cfef9198b07d
ZAWADBINSHARIF <syedzawadbinsharif@gmail.com>
2022-10-02 11:09:14 +0600

    Update employee and get one employee's info

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -8,17 +8,32 @@ const getAllEmployees = (req, res) => {
 };
 
 const createNewEmployee = (req, res) => {
-    res.json({
-        "name": req.body.name,
-        "city": req.body.city
-    });
+    const newEmployee = {
+        id: data.employees?.length ? data.employees[data.employees.length - 1].id + 1 : 1,
+        employeeName: req.body.name,
+        city: req.body.city
+    };
+
+    if (!newEmployee.employeeName || !newEmployee.city) {
+        return res.status(400).json({
+            'message': 'employeeName and city are required'
+        });
+    };
+
+    data.setEmployee([...data.employees, newEmployee]);
+    res.status(201).json(data.employees);
 };
 
 const updateEmployee = (req, res) => {
-    res.json({
-        "name": req.body.name,
-        "city": req.body.city
-    });
+    const employee = data.employees?.find(employee => employee.id === parseInt(req.body.id));
+    if (!employee) return res.status(400).json({ 'message': 'Employee not found' });
+    if (req.body.name) employee.name = req.body.name;
+    if (req.body.city) employee.city = req.body.city;
+    const filterArray = data.employees.filter(employee => employee.id !== req.body.id);
+    const unsortedArray = [...filterArray, employee];
+    const sortedArray = unsortedArray.sort((a, b) => a.id > b.id ? 1 : a.id < b.id ? -1 : 0);
+    data.setEmployee(sortedArray)
+    res.json(data.employees);
 };
 
 const deleteEmployee = (req, res) => {
@@ -28,7 +43,9 @@ const deleteEmployee = (req, res) => {
 };
 
 const getEmployee = (req, res) => {
-    res.json({ "id": req.params.id });
+    const employee = data.employees?.find(employee => employee.id === parseInt(req.params.id));
+    if (!employee) return res.status(400).json({ 'message': 'Employee not found' });
+    res.json(employee);
 };
 
 module.exports = {
