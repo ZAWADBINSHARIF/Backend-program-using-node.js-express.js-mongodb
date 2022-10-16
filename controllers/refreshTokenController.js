4c420708f52c30183285687735d6d512e5dfbcff
ZAWADBINSHARIF <syedzawadbinsharif@gmail.com>
2022-10-16 21:40:54 +0600

    fix bugs

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const jwt = require('jsonwebToken');
+const jwt = require('jsonwebtoken');
 const User = require('../model/User.js');
 
 const handleRefreshToken = async (req, res) => {
